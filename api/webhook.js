54b3b668d3880f051e159c2e7efb7918e74f5191
Manasses_M_Lira <127964940+manassesmlira@users.noreply.github.com>
2026-06-01 13:41:21 -0300

    verificacao do req.body

--- api/webhook.js
+++ api/webhook.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
   }
 
   try {
+    console.log("Conteúdo de req.body:", req.body);
     const { Nome, Numero, 'Dados do Evento': mensagemDoLead } = req.body;
 
     if (!mensagemDoLead || !Numero) {
